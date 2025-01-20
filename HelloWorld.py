3cff782320a55d0ead4c8f013fc9a783ad93c493
Denis Rukin <denisruk3@gmail.com>
2025-01-20 18:22:11 +0700

    SecondCommit

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,11 @@
-print ("Hello world")
+import tkinter as tk
+from tkinter import messagebox
+
+def show_popup():
+    root = tk.Tk()
+    root.withdraw()  # Hide the main window
+    messagebox.showinfo("Popup", "Hello, this is a popup window!")
+    root.destroy()
+
+print("Hello world")
+show_popup()
